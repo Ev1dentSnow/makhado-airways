3e230cd1c31f01aba5226f2e81753256f76df710
Ev1dentSnow <munchingfruit@gmail.com>
2023-09-25 23:42:36 +0200

    adjusted hero section

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -7,8 +7,9 @@ export default function Header() {
 	return (
 		<header className="parallax-header">
 			<Image src={MakhadoBackground} alt="Makhado Airways B738 background image" className="parallax-background" />
-			<h1 className="font-bold text-8xl tracking-wide mb-8">MAKHADO AIRWAYS</h1>
-			<h4 className="text-center text-3xl font-medium tracking-wide mb-12">&quot;South Africa&apos;s <span className="italic">most popular</span> virtual airline&quot; ~ some celebrity</h4>
+			<h4 className="text-6xl mb-8">Welcome to</h4>
+			<h1 className="font-bold text-8xl tracking-wide mb-8">Makhado Airways</h1>
+			<h4 className="text-center text-3xl font-medium tracking-wide mb-12">&quot;South Africa&apos;s <span className="italic">favourite</span> virtual airline&quot;</h4>
 			<Button variant="outline" className="w-80 h-12 text-xl" asChild>
 				<Link href="getting-started">Join The Team</Link>
 			</Button>
