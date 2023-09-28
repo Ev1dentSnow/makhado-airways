4f4c2c412e293b3d4c6785032f3fef45d3c5b2a8
Moagi <53832997+Ev1dentSnow@users.noreply.github.com>
2023-09-28 19:09:13 +0200

    add: implemented getting started page design

--- app/getting-started/page.tsx
+++ app/getting-started/page.tsx
@@ -2,6 +2,11 @@ import Instructions from "@/components/getting-started/Instructions";
 
 export default function GettingStarted() {
 	return (
-		<Instructions />
+		<div className="flex flex-col gap-12">
+			<header className="h-max pt-64 pb-48 -z-4 flex justify-center bg-muted-foreground">
+				<h1 className="text-6xl font-semibold">Getting Started</h1>
+			</header>
+			<Instructions />
+		</div>
 	);
 }
--- components/getting-started/Instructions.tsx
+++ components/getting-started/Instructions.tsx
@@ -1,31 +1,62 @@
+import { Button } from "../ui/button";
+import { BsDiscord } from "react-icons/bs";
+import { BiLinkExternal } from "react-icons/bi"
+import Link from "next/link";
+
 
 export default function Instructions() {
     return (
-        <section className="w-screen h-screen items-center flex flex-col gap-8 pt-64">
-            <h1 className="text-4xl">Makhado Airways Virtual Airline Onboarding</h1>
-            <p>
-                We are currently accepting applications and cannot wait to meet you!
-                To get started, there are 3 simple steps
-            </p>
-            <h2 className="text-2xl">Step 1</h2>
-            <p>
-                Join our discord server and introduce yourself. We're hyped to get to know
-                you.
-            </p>
-            <h2 className="text-2xl">Step 2</h2>
-            <p>
-                Head over to https://www.ftw-sim.de/ and click sign up. You will be directed
-                to a page which will show you the two-step process of creating a FlyTheWorld
-                account. The website might be in german, but you can translate it to English
-                by right clicking on the page and clicking "Translate to English"
-            </p>
-            <h2 className="text-2xl">Step 3</h2>
-            <p>
-                Keep monitoring your Aiena e.V forum account for the notification stating that
-                your FTW account has been activated. When it has, you will be able to login on
-                the actual FTW website and access your profile. To join the airline, simply 
-                request to be added as a crew member on our discord server
-            </p>
+        <section className="w-screen h-fit flex flex-col gap-16 pt-24 pb-32">
+            <div className="flex flex-col items-center gap-8 pb-6">
+                <h1 className="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[rgba(0,102,168,1)] via-[rgba(77,106,136,1)] to-[rgba(130,179,228,1)]">Makhado Airways Virtual Airline Onboarding</h1>
+                <p className="text-xl text-muted-foreground">
+                    We are currently accepting applications and can&apos;t wait to meet you!
+                    To get started, there are 3 simple steps
+                </p>
+            </div>
+            <div className="px-64 flex flex-col gap-8">
+                <h2 className="text-4xl font-semibold">Step 1 | Apply</h2>
+                <p className="text-muted-foreground">
+                    Join our Discord server and take some time to introduce yourself.<br/><br/>
+                    We're hyped to get to know you and find out if you're a good fit for our
+                    virtual airline.<br/><br/>Here we can also renderassistance with the onboarding
+                    process if you require any
+                </p>
+                <Button asChild className="h-14 w-96 bg-[#5865F2]" variant="outline">
+                    <Link href="https://discord.gg/2XsyUuE9tr" target="_blank" className="text-lg font-semibold">
+                    <BsDiscord size={24} className="mr-2" />
+                        Join our Discord server
+                    <BiLinkExternal size={24} className="ml-2" />
+                    </Link>
+                </Button>
+            </div>
+            <div className="px-64 flex flex-col gap-6">
+                <h2 className="text-4xl font-semibold">Step 2 | Account Creation</h2>
+                <p className="text-muted-foreground">
+                    Head over to the FlyTheWorld website and click sign up.<br/><br/>
+                    You will be directed to a page which will show you the two-step process 
+                    of creating a FlyTheWorld account.<br/><br/> The website might be in 
+                    german, but you can translate it to English by right clicking on the
+                    page and clicking "Translate to English"
+                </p>
+
+                    <Button asChild className="h-14 w-96 bg-primary" variant="outline">
+                    <Link href="https://www.ftw-sim.de/" target="_blank" className="text-lg font-semibold">
+                        FlyTheWorld VA Platform Website
+                    <BiLinkExternal size={24} className="ml-2" />
+                    </Link>
+                </Button>
+
+            </div>
+            <div className="px-64 flex flex-col gap-6">
+                <h2 className="text-4xl font-semibold">Step 3 | Joining The Pilot Roster</h2>
+                <p className="text-muted-foreground">
+                    Keep monitoring your Aiena e.V forum account for the notification stating that
+                    your FTW account has been activated.<br/><br/>When your account has been activated,
+                    you will be able to login on the actual FTW website and access your profile.<br/><br/>
+                    To join the airline, simply request to be added as a crew member on our discord server
+                </p>
+            </div>
         </section>
     )
 }
