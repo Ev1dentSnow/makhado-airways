2badd1f06b3dae3b1f4aa02a6cabe052926268f1
Ev1dentSnow <munchingfruit@gmail.com>
2023-10-01 02:02:18 +0200

    MORE footer work

--- components/footer/FooterContent.tsx
+++ components/footer/FooterContent.tsx
@@ -1,18 +1,29 @@
+import Image from "next/image";
+import VATSIMLogo from "../../public/VATSIMLogo.png";
+import VATSSALogo from "../../public/VATSSALogo.png";
 
 export default function FooterContent() {
     return (
-        <div className="flex flex-col gap-6 items-center pt-6">
+        <div className="flex flex-col gap-6 items-center pt-6 bg-black">
             <div>
                 <h5>Disclaimer: Makhado Airways is a virtual airline for flight simulators only. We do not exist in the real world</h5>
             </div>
-            <div className="flex gap-6">
-                <div>
-                    <h5>Compatible Simulators</h5>
-                </div>
-                <div>
-                    <h5>Our Partners</h5>
-                </div>
+            <div>
+                <h5 className="text-2xl">Our Partners</h5>
             </div>
+            <div className="flex gap-12">
+                <Image 
+                    src={VATSIMLogo}
+                    alt="VATSIM Network Logo"
+                    width={256}
+                />
+                <Image 
+                    src={VATSSALogo}
+                    alt="VATSSA Logo"
+                    width={256}
+                />
+            </div>
+        
 
         </div>
     )
