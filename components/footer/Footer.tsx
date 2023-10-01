baa909f73dc0921a7ffe339c603853dcb7a4adc3
Ev1dentSnow <munchingfruit@gmail.com>
2023-10-01 23:32:05 +0200

    Added our partners and copyright to footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,29 +1,46 @@
 import Image from "next/image";
 import VATSIMLogo from "../../public/VATSIMLogo.png";
 import VATSSALogo from "../../public/VATSSALogo.png";
+import FTWLogo from "../../public/FTWLogo.png";
+import Link from "next/link";
 
 export default function Footer() {
     return (
-        <div className="flex flex-col gap-6 items-center pt-6 bg-black">
-            <div>
-                <h5>Disclaimer: Makhado Airways is a virtual airline for flight simulators only. We do not exist in the real world</h5>
-            </div>
-            <div>
-                <h5 className="text-2xl">Our Partners</h5>
-            </div>
+        <div className="flex flex-col gap-6 items-center pt-6 pb-4 bg-background">
+            <h6 className="text-2xl">Our Partners</h6>
             <div className="flex gap-12">
-                <Image 
-                    src={VATSIMLogo}
-                    alt="VATSIM Network Logo"
-                    width={256}
-                />
-                <Image 
-                    src={VATSSALogo}
-                    alt="VATSSA Logo"
-                    width={256}
-                />
+                <div>
+                    <Link href="https://vatsim.net/" target="_blank">
+                        <Image 
+                        src={VATSIMLogo}
+                        alt="VATSIM Network Logo"
+                        width={128}
+                        />
+                    </Link>
+                    </div>
+                <div>
+                    <Link href="https://vatssa.com/hq/index.php" target="_blank">
+                        <Image 
+                        src={VATSSALogo}
+                        alt="VATSSA Logo"
+                        width={128}
+                        />
+                    </Link>
+                </div>
+                <div>
+                    <Link href="https://www.ftw-sim.de/" target="_blank">
+                        <Image
+                        src={FTWLogo}
+                        alt="FlyTheWorld Virtual Airline Platform Logo"
+                        width={64}
+                        />
+                    </Link>
+                </div>
             </div>
-        
+            <div className="flex flex-col items-center gap-6">
+                <p className="text-muted-foreground">Copyright Makhado Airways Virtual Airline. All Rights Reserved</p>
+                <p className="text-muted-foreground">Background images supplied with &#9825; from Skhindi</p>
+            </div>
 
         </div>
     )
