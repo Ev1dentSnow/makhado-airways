3cfd002ada36e93f4a6d585cfe0b2b979ebeb26f
Ev1dentSnow <munchingfruit@gmail.com>
2023-10-02 01:22:09 +0200

    Made features page mobile responsive

--- components/home/Features.tsx
+++ components/home/Features.tsx
@@ -15,12 +15,13 @@ export default function Features() {
     }
 
     return (
-        <section className="flex gap-48 pl-24 pr-24 pt-12 pb-12">
+        <section className="flex flex-col sm:flex-row gap-24 sm:gap-48 px-6 sm:px-24 pt-12 pb-12">
             <div className="flex flex-col justify-center items-center gap-8 min-w-1/3">
                 <h2 className="text-5xl font-bold">Crafted to ensure an <span className="text-transparent bg-clip-text bg-gradient-to-r from-[rgba(0,102,168,1)] via-[rgba(77,106,136,1)] to-[rgba(130,179,228,1)]">authentic virtual aviation experience</span></h2>
                 <p className="text-lg">Here, we dont have competition. We operate on a completly different wavelength from the competition, thus we are not the same</p>
             </div>
-            <div className="grid grid-rows-2 grid-cols-2 gap-8">
+
+            <div className="grid grid-rows-2 sm:grid-cols-2 gap-8">
                 <div className="flex flex-col gap-6 p-4 rounded-md shadow-2xl hover:bg-accent transition-all duration-300">
                     <div className="w-fit rounded-md p-2 bg-[rgba(130,179,228,1)] border-white">
                         <Laptop size={40} color={setFeatureIconColor()} />
