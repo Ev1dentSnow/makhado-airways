15123afc17d026738725aa32feb1e55e34801dbd
Ev1dentSnow <munchingfruit@gmail.com>
2023-10-04 23:17:34 +0200

    Added Discord Authentication skeleton

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,12 @@
+import {SessionProvider} from "next-auth/react";
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div>
-			<h1>This is the dashboards navbar</h1>
-			{children}
-		</div>
+		<SessionProvider>
+			<div>
+				<h1>This is the dashboards navbar</h1>
+				{children}
+			</div>
+		</SessionProvider>
 	);
 }
